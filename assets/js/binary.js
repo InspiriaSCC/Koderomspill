9fbd2692cbea36535cce7457c0bf5891fb9678a1
InspiriaSCC <98588388+InspiriaSCC@users.noreply.github.com>
2023-02-27 16:08:08 +0100

    0.0.2

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.22/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.1","target":"arcade","targetVersion":"1.12.22","repo":"inspiriascc/koderomspill"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.22/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.2","target":"arcade","targetVersion":"1.12.22","repo":"inspiriascc/koderomspill"}
 // total=1253309 new=5.66% cached=88.69% other=5.64%
 (function (ectx) {
 'use strict';
@@ -423,7 +423,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P106510(s) {
+function _main___P106653(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -818,13 +818,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P106510.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P106510.continuations = [  ]
+_main___P106653.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P106653.continuations = [  ]
 
-function _main___P106510_mk(s) {
+function _main___P106653_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P106510, depth: s.depth + 1,
+        parent: s, fn: _main___P106653, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42415,5 +42415,5 @@ const sprites_BaseSpriteSayRenderer__C90810_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P106510
+return _main___P106653
 })
